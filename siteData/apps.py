fcf777a118d41b5a4ba9b24b8fba736da2bfd9b6
henishpatel9045 <200120107182@git.org.in>
2023-03-01 09:27:24 +0530

    ..

--- siteData/apps.py
+++ siteData/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class SitedataConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
+    path = "/home/persistanceEdu/persistanceEduBackend/siteData"
     name = 'siteData'
